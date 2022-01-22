781b2b2f56708e4f59e0015e161c123f8ace08d1
Biao Yi <schoolex@hotmail.com>
2022-01-23 01:23:28 +0800

    Add address to UI

--- src/components/common/Layout/FunctionalButtons/FunctionalButtons.tsx
+++ src/components/common/Layout/FunctionalButtons/FunctionalButtons.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import FriendsButton from './FriendsButton';
 import LogoutButton from './LogoutButton';
 import NotificationsBell from './NotificationsBell';
+import WalletInfo from './WalletInfo';
 
 const FunctionalButtons: React.FC = () => {
     return (
         <>
+            <WalletInfo />
             <NotificationsBell />
             <FriendsButton />
             <LogoutButton />
