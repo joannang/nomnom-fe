c26f80febbfd6f6dde160d16b9eddad356519daa
runlinnn <chiarunlin@gmail.com>
2022-01-23 14:59:11 +0800

    removed vouchers

--- ethereum/contracts/Market.sol
+++ ethereum/contracts/Market.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.0;
 
 import './Supplier.sol';
 import './Food.sol';
-import './Voucher.sol';
 
 contract Market {
 
@@ -31,28 +30,6 @@ contract Market {
         uint256 tokenID
     );
 
-    event EventBoughtVoucher(
-        address indexed user,
-        string voucherName,
-        uint256 date,
-        uint256 tokenID
-    );
-
-    event EventRedeemVoucher(
-        address indexed user,
-        string voucherName,
-        uint256 date,
-        uint256 tokenID
-    );
-
-    event EventGiftVoucher(
-        address indexed user,
-        address indexed receiver,
-        string voucherName,
-        uint256 date,
-        uint256 tokenID
-    );
-
     struct Token {
         string name;
         uint256 tokenID;
@@ -69,7 +46,6 @@ contract Market {
 
     Supplier public supplier;
     Food public food;
-    Voucher public voucher;
 
     mapping (address => Customer) private customers;
 
@@ -78,7 +54,6 @@ contract Market {
     constructor() payable {
         supplier = new Supplier();
         food = new Food();
-        voucher = new Voucher();
     }
 
     function buyFood(string memory foodName) public payable returns (uint256){
@@ -129,80 +104,8 @@ contract Market {
         }
     }
 
-    function buyFoodPromo(string memory foodName, uint256 voucherID) public payable returns(uint256){
-        uint256 originalPrice = supplier.getFoodPrice(foodName);
-        string memory voucherName = voucher.tokenURI(voucherID);
-        uint256 discountedPrice = originalPrice - supplier.getVoucherValue(voucherName);
-        require(msg.value >= discountedPrice, "Insufficient money");
-        uint256 tokenID = food.buy(msg.sender, foodName); // mint token
-        Token memory foodToken = Token({
-            name: foodName,
-            tokenID: tokenID
-        });
-        customers[msg.sender].ownedFoods.push(foodToken); // track all customer tokens
-        lastTokenID = tokenID;
-        emit EventBoughtFood(msg.sender, foodName, block.timestamp, tokenID);
-        return tokenID;
-    }
-
-    function buyAndGiftFoodPromo(string memory foodName, uint256 voucherID, address receiver) public payable {
-        uint256 tokenID = buyFoodPromo(foodName, voucherID);
-        giftFood(receiver, tokenID);
-    }
-
-    function buyVoucher(string memory voucherName) public payable returns (uint256){
-        uint256 price = supplier.getVoucherValue(voucherName);
-        require(msg.value >= price, "Insufficient money");
-        uint256 tokenID = voucher.buy(msg.sender, voucherName); // mint token
-        Token memory voucherToken = Token({
-            name: voucherName,
-            tokenID: tokenID
-        });
-        voucherTokens[tokenID] = voucherToken; // track all tokens
-        customers[msg.sender].ownedVouchers.push(voucherToken); // track all customer tokens
-        lastTokenID = tokenID;
-        emit EventBoughtFood(msg.sender, voucherName, block.timestamp, tokenID);
-        return tokenID;
-    }
-
-    function redeemVoucher(uint256 tokenID) public {
-        revokeVoucherOwnership(msg.sender, tokenID);
-        voucher.redeem(msg.sender, tokenID);
-        Token storage voucherToken = voucherTokens[tokenID];
-        emit EventRedeemFood(msg.sender, voucherToken.name, block.timestamp, tokenID);
-    }
-
-    function giftVoucher(address receiver, uint256 tokenID) public {
-        revokeVoucherOwnership(msg.sender, tokenID);
-        voucher.gift(msg.sender, receiver, tokenID);
-        Token storage voucherToken = voucherTokens[tokenID];
-        customers[receiver].ownedFoods.push(voucherToken);
-        emit EventGiftFood( msg.sender, receiver, voucherToken.name, block.timestamp, tokenID);
-    }
-
-    function buyAndGiftVoucher(string memory voucherName, address receiver) public payable {
-        uint256 tokenID = buyVoucher(voucherName);
-        giftVoucher(receiver, tokenID);
-    }
-
-    function revokeVoucherOwnership(address owner, uint256 tokenID) private {
-        uint256 oldTokenBalance = voucher.balanceOf(msg.sender);
-        Customer storage customer = customers[owner];
-        Token[] storage ownedVouchers = customer.ownedVouchers;
-        for (uint256 i = 0; i < oldTokenBalance; i++) {
-            if (ownedVouchers[i].tokenID == tokenID) {
-                ownedVouchers[i] = ownedVouchers[oldTokenBalance - 1];
-                ownedVouchers.pop();
-                break;
-            }
-        }
-    }
-
     function getCustomerFoods() public view returns (Token[] memory) {
         return customers[msg.sender].ownedFoods;
     }
 
-    function getCustomerVouchers() public view returns (Token[] memory) {
-        return customers[msg.sender].ownedVouchers;
-    }
 }
